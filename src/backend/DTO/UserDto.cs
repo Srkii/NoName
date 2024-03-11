e30d386c46be5461b0b36b5169b12427f2207754
Tihomir Rajicic <tihomir.rajicic@gmail.com>
2024-03-11 08:49:47 +0100

    #32 implementirano da kad se korisnik uloguje front dobija i njegov role

--- src/backend/DTO/UserDto.cs
+++ src/backend/DTO/UserDto.cs
@@ -1,9 +1,12 @@
-﻿namespace backend.DTO
+﻿using backend.Entities;
+
+namespace backend.DTO
 {
     public class UserDto
     {
         public int Id { get; set; }
         public string Email{get; set;}
         public string Token{get; set;}
+        public UserRole Role { get; set; }
     }
 }
