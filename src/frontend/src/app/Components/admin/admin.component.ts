ab5e9914fcc4537f2ec30781cf1c6ed00f8d71fe
miljana.bjelic <28-2020@pmf.kg.ac.rs>
2024-03-12 18:12:11 +0100

    #29 dodata logika kojim admin salje invite email korisniku

--- src/frontend/src/app/Components/admin/admin.component.ts
+++ src/frontend/src/app/Components/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AdminService } from '../../Services/admin.service';
+import { Invatation } from '../../Entities/Invatation';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
+  constructor(private adminService:AdminService ){}
+
+  invatation:Invatation={
+    reciever: ''
+  }
+
+  Invite(): void{
+    this.adminService.sendInvatation(this.invatation).subscribe({next:(res)=>{
+      console.log("Email successful sent")
+    },error:()=>{
+      console.log("Email is not sent")
+    }})
+  }
 }
--- src/frontend/src/app/Services/admin.service.ts
+++ src/frontend/src/app/Services/admin.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiUrl } from '../ApiUrl/ApiUrl';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AdminService {
 
   constructor(private httpClient:HttpClient) { }
 
-  private apiUrl=ApiUrl.apiUrl+'/users';
+  private apiUrl=ApiUrl.apiUrl;
 
   check():boolean{
     const role=localStorage.getItem('role')
@@ -19,5 +20,8 @@ export class AdminService {
     else return false
   }
 
+  sendInvatation(invData:any):Observable<any>{
+    return this.httpClient.post(`${this.apiUrl}/email/send`,invData)
+  }
 
 }
