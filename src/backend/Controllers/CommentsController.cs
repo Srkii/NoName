196ca14f170b8ecb9f89909d9bc1d105d12861b1
Srdjan Filipovic <srdjanfilipovic80@gmail.com>
2024-03-23 20:23:03 +0100

    #53 Dodat kontroler za komentare

---
+++ src/backend/Controllers/CommentsController.cs
@@ -0,0 +1,39 @@
+﻿
+using backend.Data;
+using backend.DTO;
+using backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers;
+
+public class CommentsController:BaseApiController
+{
+    private readonly DataContext context;
+    public CommentsController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpPost("postComment")] // /api/comments/postComment
+    public async Task<IActionResult> PostComment(CommentDto commentDto)
+    {
+        var comment = new Comment
+        {
+            TaskId = commentDto.TaskId,
+            Content = commentDto.Content,
+            SenderFirstName = commentDto.SenderFirstName,
+            SenderLastName = commentDto.SenderLastName
+        };
+        context.Comments.Add(comment);
+        await context.SaveChangesAsync();
+        return Ok(comment);
+    }
+
+    [HttpGet("getComments/{taskId}")] // /api/comments/getComments
+    public async Task<IActionResult> GetComments(int taskId)
+    {
+        var comments = await context.Comments.Where(x => x.TaskId == taskId).OrderByDescending(x => x.MessageSent).ToListAsync();
+        return Ok(comments);
+    }
+}
--- src/backend/DTO/CommentDto.cs
+++ src/backend/DTO/CommentDto.cs
@@ -1,11 +1,9 @@
-﻿namespace backend.Entities;
+﻿namespace backend.DTO;
 
-public class Comment
+public class CommentDto
 {
-    public int Id {get; set;}
     public int TaskId {get; set;}
     public string Content {get; set;}
-    public DateTime MessageSent {get; set;} = DateTime.UtcNow;
     public string SenderFirstName {get; set;}
     public string SenderLastName {get; set;}
 }
