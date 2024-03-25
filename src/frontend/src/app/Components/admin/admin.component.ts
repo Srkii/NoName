4523e1b7c721b4cf52af087b30a2f26bff1a702e
miljana.bjelic <28-2020@pmf.kg.ac.rs>
2024-03-26 00:47:10 +0100

    #47 implementacija da admin vidi samo manage users i ispravke za invitation

--- src/frontend/src/app/Components/admin/admin.component.ts
+++ src/frontend/src/app/Components/admin/admin.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AdminService } from '../../_services/admin.service';
-import { Invatation } from '../../Entities/Invatation';
+import { Invatation } from '../../Entities/RegisterInvitation';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
