dc865c5c31b6792624865fcab1e841731387719f
Tihomir Rajicic <tihomir.rajicic@gmail.com>
2024-03-31 16:23:22 +0200

    #64 doradjen projectDetails table view

--- src/frontend/src/app/Components/project-detail/project-detail.component.ts
+++ src/frontend/src/app/Components/project-detail/project-detail.component.ts
@@ -15,7 +15,7 @@ export class ProjectDetailComponent implements OnInit {
   project: Project | undefined;
   projectTasks: ProjectTask[] = [];
   viewMode: string = 'table';
-  groupedTasks: { [key: string]: any[] } = {};
+  groupedTasks: { [key: string]: any } = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -36,27 +36,40 @@ export class ProjectDetailComponent implements OnInit {
         this.project = project;
         this.myTasksService.GetTasksByProjectId(project.id).subscribe((tasks) => {
           this.projectTasks = tasks;
-          this.groupedTasks = this.groupTasksBySection(tasks); // Store grouped tasks in a component property
-          console.log(this.groupedTasks);
-          console.log(this.projectTasks);
+          this.groupedTasks = this.groupTasksBySection(tasks);
         });
         this.spinner.hide();
       });
     }
   }
 
-  groupTasksBySection(tasks: any[]): { [key: string]: any[] } {
+  groupTasksBySection(tasks: any[]): { [key: string]: any } {
     const grouped = tasks.reduce((acc, task) => {
       const section = task.sectionName || 'No Section';
       if (!acc[section]) {
-        acc[section] = [];
+        acc[section] = { tasks: [], visible: section === 'No Section' };
       }
-      acc[section].push(task);
+      acc[section].tasks.push(task);
       return acc;
     }, {});
   
+    // Ensure "No Section" is always visible
+    if (!grouped['No Section']) {
+      grouped['No Section'] = { tasks: [], visible: true };
+    }
+
     return grouped;
   }
+
+  toggleSectionVisibility(section: string): void {
+    this.groupedTasks[section].visible = !this.groupedTasks[section].visible;
+  }
+
+  sectionOrder = (a: { key: string }, b: { key: string }) => {
+    if (a.key === 'No Section') return -1;
+    if (b.key === 'No Section') return 1;
+    return a.key.localeCompare(b.key);
+  };
 
   changeToTable() {
     this.getProjectInfo();
