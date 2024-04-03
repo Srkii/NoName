9879f0f1d40b31185d998a25599eb3ec9db9d774
miljana.bjelic <28-2020@pmf.kg.ac.rs>
2024-04-03 16:54:18 +0200

    #60 kreiranje klasa na beku za pagination

---
+++ src/backend/Helpers/PaginationHeader.cs
@@ -0,0 +1,15 @@
+public class PaginationHeader{
+
+    public PaginationHeader(int CurrentPage, int ItemsPerPage, int TotalItems, int TotalPages)
+    {
+        this.CurrentPage=CurrentPage;
+        this.ItemsPerPage=ItemsPerPage;
+        this.TotalPages=TotalPages;
+        this.TotalItems=TotalItems;
+    }
+
+    public int CurrentPage { get; set; }
+    public int ItemsPerPage { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages { get; set; }
+}
